2f84d98e561c85fc221770474838cf3b5576523f
Vitaliy Motsyk <2radist@gmail.com>
2026-02-17 16:39:48 +0100

    add all js file

---
+++ js/task-1.js
@@ -0,0 +1,6 @@
+function makeTransaction(quantity, pricePerDroid) {
+    return quantity * pricePerDroid;
+}
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500)); 
